7cd2ce350ab80c1be2093ab5c3549021ed7b2983
Fabian Hobi <fabian.hobi@bluewin.ch>
2026-05-27 22:14:23 +0200

    Improved the win scene.

--- src/game/scenes/WinScene.ts
+++ src/game/scenes/WinScene.ts
@@ -1,4 +1,4 @@
-import { Scene, Sound } from 'phaser';
+import { Scene, Sound, GameObjects } from 'phaser';
 import gameOptions from "../helper/gameOptions";
 import {WinSceneData} from "../helper/types";
 
@@ -8,6 +8,17 @@ export default class WinScene extends Scene {
     private winData!: WinSceneData
     private soundtrack!: Sound.WebAudioSound;
     private fading!: boolean;
+    private titleText!: GameObjects.BitmapText;
+    private descriptionText!: GameObjects.BitmapText;
+    private leftBowlText!: GameObjects.BitmapText;
+    private rightBowlText!: GameObjects.BitmapText;
+    private timeText!: GameObjects.BitmapText;
+    private scoreText!: GameObjects.BitmapText;
+    private leftBowlValue!: GameObjects.BitmapText;
+    private rightBowlValue!: GameObjects.BitmapText;
+    private timeValue!: GameObjects.BitmapText;
+    private scoreValue!: GameObjects.BitmapText;
+    private clickText!: GameObjects.BitmapText;
 
     // Constructor
     constructor() {
@@ -35,29 +46,165 @@ export default class WinScene extends Scene {
 
         // background image
         this.add.image(0, 0, 'win').setOrigin(0);
-        this.add.rectangle(0, 0, this.scale.width, this.scale.height, 0x000000, 0.4).setOrigin(0);
 
         // Texts
-        this.add.bitmapText(0.5 * this.scale.width, this.scale.height * 0.15, 'minogram', 'Congratulations!', 30).setOrigin(0.5).setDropShadow(gameOptions.textShadowSettings.horizontalOffset, gameOptions.textShadowSettings.verticalOffset, gameOptions.textShadowSettings.color, gameOptions.textShadowSettings.alpha);
-        this.add.bitmapText(0.5 * this.scale.width, this.scale.height * 0.30, 'minogram', 'You sorted all crystals', 20).setOrigin(0.5).setDropShadow(gameOptions.textShadowSettings.horizontalOffset, gameOptions.textShadowSettings.verticalOffset, gameOptions.textShadowSettings.color, gameOptions.textShadowSettings.alpha);
+        this.titleText = this.add.bitmapText(1.05 * this.scale.width, this.scale.height * 0.15, 'minogram', 'Congratulations!', 30).setOrigin(0, 0.5).setDropShadow(gameOptions.textShadowSettings.horizontalOffset, gameOptions.textShadowSettings.verticalOffset, gameOptions.textShadowSettings.color, gameOptions.textShadowSettings.alpha);
+        this.descriptionText = this.add.bitmapText(1.05 * this.scale.width, this.scale.height * 0.27, 'minogram', 'You sorted all crystals', 20).setOrigin(0, 0.5).setDropShadow(gameOptions.textShadowSettings.horizontalOffset, gameOptions.textShadowSettings.verticalOffset, gameOptions.textShadowSettings.color, gameOptions.textShadowSettings.alpha);
 
-        const startY = 0.45 * this.scale.height;
+        const startY = 0.42 * this.scale.height;
         const distanceY = 0.1 * this.scale.height;
-        let startX = 0.25 * this.scale.width;
+        let startX = 1.05 * this.scale.width;
 
-        this.add.bitmapText(startX, startY, 'minogram', 'Left bowl:', 20).setOrigin(0, 0.5).setDropShadow(gameOptions.textShadowSettings.horizontalOffset, gameOptions.textShadowSettings.verticalOffset, gameOptions.textShadowSettings.color, gameOptions.textShadowSettings.alpha);
-        this.add.bitmapText(startX, startY + distanceY, 'minogram', 'Right bowl:', 20).setOrigin(0, 0.5).setDropShadow(gameOptions.textShadowSettings.horizontalOffset, gameOptions.textShadowSettings.verticalOffset, gameOptions.textShadowSettings.color, gameOptions.textShadowSettings.alpha);
-        this.add.bitmapText(startX, startY + 2 * distanceY, 'minogram', 'Time:', 20).setOrigin(0, 0.5).setDropShadow(gameOptions.textShadowSettings.horizontalOffset, gameOptions.textShadowSettings.verticalOffset, gameOptions.textShadowSettings.color, gameOptions.textShadowSettings.alpha);
-        this.add.bitmapText(startX, startY + 3 * distanceY, 'minogram', 'Score:', 20).setOrigin(0, 0.5).setDropShadow(gameOptions.textShadowSettings.horizontalOffset, gameOptions.textShadowSettings.verticalOffset, gameOptions.textShadowSettings.color, gameOptions.textShadowSettings.alpha);
+        this.leftBowlText = this.add.bitmapText(startX, startY, 'minogram', 'Left bowl:', 20).setOrigin(0, 0.5).setDropShadow(gameOptions.textShadowSettings.horizontalOffset, gameOptions.textShadowSettings.verticalOffset, gameOptions.textShadowSettings.color, gameOptions.textShadowSettings.alpha);
+        this.rightBowlText = this.add.bitmapText(startX, startY + distanceY, 'minogram', 'Right bowl:', 20).setOrigin(0, 0.5).setDropShadow(gameOptions.textShadowSettings.horizontalOffset, gameOptions.textShadowSettings.verticalOffset, gameOptions.textShadowSettings.color, gameOptions.textShadowSettings.alpha);
+        this.timeText = this.add.bitmapText(startX, startY + 2 * distanceY, 'minogram', 'Time:', 20).setOrigin(0, 0.5).setDropShadow(gameOptions.textShadowSettings.horizontalOffset, gameOptions.textShadowSettings.verticalOffset, gameOptions.textShadowSettings.color, gameOptions.textShadowSettings.alpha);
+        this.scoreText = this.add.bitmapText(startX, startY + 3.7 * distanceY, 'minogram', 'Score:', 30).setOrigin(0, 0.5).setDropShadow(gameOptions.textShadowSettings.horizontalOffset, gameOptions.textShadowSettings.verticalOffset, gameOptions.textShadowSettings.color, gameOptions.textShadowSettings.alpha);
 
-        startX = 0.6 * this.scale.width;
+        startX = 1.40 * this.scale.width;
 
-        this.add.bitmapText(startX, startY, 'minogram', this.winData.leftBowlEE.toFixed(0) + ' %ee', 20).setOrigin(0, 0.5).setDropShadow(gameOptions.textShadowSettings.horizontalOffset, gameOptions.textShadowSettings.verticalOffset, gameOptions.textShadowSettings.color, gameOptions.textShadowSettings.alpha);
-        this.add.bitmapText(startX, startY + distanceY, 'minogram', this.winData.rightBowlEE.toFixed(0) + ' %ee', 20).setOrigin(0, 0.5).setDropShadow(gameOptions.textShadowSettings.horizontalOffset, gameOptions.textShadowSettings.verticalOffset, gameOptions.textShadowSettings.color, gameOptions.textShadowSettings.alpha);
-        this.add.bitmapText(startX, startY + 2 * distanceY, 'minogram', this.winData.time, 20).setOrigin(0, 0.5).setDropShadow(gameOptions.textShadowSettings.horizontalOffset, gameOptions.textShadowSettings.verticalOffset, gameOptions.textShadowSettings.color, gameOptions.textShadowSettings.alpha);
-        this.add.bitmapText(startX, startY + 3 * distanceY, 'minogram', this.winData.score.toFixed(0), 20).setOrigin(0, 0.5).setDropShadow(gameOptions.textShadowSettings.horizontalOffset, gameOptions.textShadowSettings.verticalOffset, gameOptions.textShadowSettings.color, gameOptions.textShadowSettings.alpha);
+        this.leftBowlValue = this.add.bitmapText(startX, startY, 'minogram', this.winData.leftBowlEE.toFixed(0) + ' %ee', 20).setOrigin(0, 0.5).setDropShadow(gameOptions.textShadowSettings.horizontalOffset, gameOptions.textShadowSettings.verticalOffset, gameOptions.textShadowSettings.color, gameOptions.textShadowSettings.alpha);
+        this.rightBowlValue = this.add.bitmapText(startX, startY + distanceY, 'minogram', this.winData.rightBowlEE.toFixed(0) + ' %ee', 20).setOrigin(0, 0.5).setDropShadow(gameOptions.textShadowSettings.horizontalOffset, gameOptions.textShadowSettings.verticalOffset, gameOptions.textShadowSettings.color, gameOptions.textShadowSettings.alpha);
+        this.timeValue = this.add.bitmapText(startX, startY + 2 * distanceY, 'minogram', this.winData.time, 20).setOrigin(0, 0.5).setDropShadow(gameOptions.textShadowSettings.horizontalOffset, gameOptions.textShadowSettings.verticalOffset, gameOptions.textShadowSettings.color, gameOptions.textShadowSettings.alpha);
+        this.scoreValue = this.add.bitmapText(startX, startY + 3.7 * distanceY, 'minogram', this.winData.score.toFixed(0), 30).setOrigin(0, 0.5).setDropShadow(gameOptions.textShadowSettings.horizontalOffset, gameOptions.textShadowSettings.verticalOffset, gameOptions.textShadowSettings.color, gameOptions.textShadowSettings.alpha);
 
-        this.add.bitmapText(0.5 * this.scale.width, this.scale.height * 0.9, 'minogram', 'Click anywhere to go back', 10).setOrigin(0.5).setDropShadow(gameOptions.textShadowSettings.horizontalOffset, gameOptions.textShadowSettings.verticalOffset, gameOptions.textShadowSettings.color, gameOptions.textShadowSettings.alpha);
+        this.clickText =  this.add.bitmapText(0.33 * this.scale.width, this.scale.height * 1.13, 'minogram', 'Click anywhere to go back', 10).setOrigin(0.5).setDropShadow(gameOptions.textShadowSettings.horizontalOffset, gameOptions.textShadowSettings.verticalOffset, gameOptions.textShadowSettings.color, gameOptions.textShadowSettings.alpha);
+
+
+        // move in objects
+        this.moveInObjects();
+
+        // ensure the music is really stopped
+        this.events.once('shutdown', () => {
+            this.sound.get('soundtrackGame').stop();
+        });
+
+
+    }
+
+    // move in objects
+    moveInObjects() {
+
+        this.add.timeline([
+            {
+                at: gameOptions.fadeInOutTime,
+                tween: {
+                    targets: this.titleText,
+                    x: 0.05 * this.scale.width,
+                    ease: 'Cubic.easeOut',
+                    duration: 750
+                }
+            },
+            {
+                from: 250,
+                tween: {
+                    targets: this.descriptionText,
+                    x: 0.05 * this.scale.width,
+                    ease: 'Cubic.easeOut',
+                    duration: 750
+                }
+            },
+            {
+                from: 750,
+                tween: {
+                    targets: this.leftBowlText,
+                    x: 0.05 * this.scale.width,
+                    ease: 'Cubic.easeOut',
+                    duration: 750
+                }
+            },
+            {
+                from: 250,
+                tween: {
+                    targets: this.rightBowlText,
+                    x: 0.05 * this.scale.width,
+                    ease: 'Cubic.easeOut',
+                    duration: 750
+                }
+            },
+            {
+                from: 250,
+                tween: {
+                    targets: this.timeText,
+                    x: 0.05 * this.scale.width,
+                    ease: 'Cubic.easeOut',
+                    duration: 750
+                }
+            },
+            {
+                from: 750,
+                tween: {
+                    targets: this.leftBowlValue,
+                    x: 0.4 * this.scale.width,
+                    ease: 'Cubic.easeOut',
+                    duration: 750
+                }
+            },
+            {
+                from: 250,
+                tween: {
+                    targets: this.rightBowlValue,
+                    x: 0.4 * this.scale.width,
+                    ease: 'Cubic.easeOut',
+                    duration: 750
+                }
+            },
+            {
+                from: 250,
+                tween: {
+                    targets: this.timeValue,
+                    x: 0.4 * this.scale.width,
+                    ease: 'Cubic.easeOut',
+                    duration: 750
+                }
+            },
+            {
+                from: 750,
+                tween: {
+                    targets: this.scoreText,
+                    x: 0.05 * this.scale.width,
+                    ease: 'Cubic.easeOut',
+                    duration: 750
+                }
+            },
+            {
+                from: 750,
+                tween: {
+                    targets: this.scoreValue,
+                    x: 0.40 * this.scale.width,
+                    ease: 'Cubic.easeOut',
+                    duration: 750
+                }
+            },
+            {
+                from: 750,
+                tween: {
+                    targets: this.scoreValue,
+                    scale: 1.5,
+                    ease: 'Cubic.easeOut',
+                    yoyo: true,
+                    duration: 500
+                }
+            },
+            {
+                from: 1000,
+                tween: {
+                    targets: this.clickText,
+                    y: 0.93 * this.scale.height,
+                    ease: 'Cubic.easeOut',
+                    duration: 500
+                }
+            },
+            {
+                from: 500,
+                run: () => {
+                    this.enableClick();
+                }
+            },
+        ]).play();
+
+    }
+
+    enableClick() {
 
         // Click event
         this.input.on('pointerdown', () => {
@@ -82,15 +229,7 @@ export default class WinScene extends Scene {
             }
 
         });
-
-        // ensure the music is really stopped
-        this.events.once('shutdown', () => {
-            this.sound.get('soundtrackGame').stop();
-        });
-
-
     }
-
 
 
 }
